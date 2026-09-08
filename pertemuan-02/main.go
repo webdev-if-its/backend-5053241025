c772013c7f5195e5b52402bacd6a58f63cb8b709
Dex <140959059+AlsDei@users.noreply.github.com>
2026-09-08 17:45:18 +0700

    pertemuan-02: level 7 selesai

--- pertemuan-02/main.go
+++ pertemuan-02/main.go
@@ -68,7 +68,14 @@ func ValidasiPesanan(qty []int, hargaSatuan []float64) (bool, string) {
 }
 
 func TentukanStatus(total float64) string {
-	panic("belum diimplementasikan")
+	switch {
+	case total > 1000000:
+		return "Prioritas"
+	case total <= 1000000 && total > 100000:
+		return "Reguler"
+	default:
+		return "Hemat"
+	}
 }
 
 func RingkasanPesanan(qty []int, hargaSatuan []float64, tarifPajak float64) string {
