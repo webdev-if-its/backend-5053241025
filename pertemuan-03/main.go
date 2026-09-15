e04bff0ca73ac5c2f2be47a771df30be98c43af3
Dex <140959059+AlsDei@users.noreply.github.com>
2026-09-15 17:59:12 +0700

    pertemuan-03: level 8 selesai

--- pertemuan-03/main.go
+++ pertemuan-03/main.go
@@ -92,7 +92,13 @@ func HapusTugasTercatat(toko *TokoTugas, id int) error {
 // menangkapnya lewat recover dan mengembalikannya sebagai error biasa,
 // alih-alih membiarkan panic itu merambat dan menghentikan program.
 func AmankanPanggilan(fn func() error) (err error) {
-	panic("belum diimplementasikan")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Successfully recovered from panic:", r)
+			err = fmt.Errorf("operasi gagal: %v", r)
+		}
+	}()
+	return fn()
 }
 
 // AmankanHandler membungkus next: kalau next panic saat memproses satu
